a2b3ea232481435c4770f8428e40d866f131b801
Dajeong Choe <hers010801@gmail.com>
2022-11-02 23:08:56 +0900

    BaekJoon #3190

--- CodingTestStudy1.0/src/Problem3190.java
+++ CodingTestStudy1.0/src/Problem3190.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Problem3190 {
@@ -17,27 +18,37 @@ public class Problem3190 {
 		}
 		
 		int L = scan.nextInt();	//뱀의 방향 변환 횟수
-		int X = 0;
-		char C = 0 ;
+		//int X = 0;
+		//char C = 0 ;
+		HashMap<String, Object> map = new HashMap<String, Object>();
 		for (int i = 0; i < L; i++) {
-			X = scan.nextInt();		//게임 경과시간
-			C = scan.next().charAt(0);	//회전 방향
+			map.put("X"+i, scan.nextInt());		//게임 경과시간
+			map.put("C"+i, scan.next().charAt(0));	//회전 방향
+			//X = scan.nextInt();		
+			//C = scan.next().charAt(0);	//회전 방향
 		}
 		
 		
 		int l = 1;	//뱀의 길이
 		int cnt = 1;	//시간 소요
-		int[][] lotate = new int[y][x];
+		//board 영역 생성
 		for (int i = 0 ; i < N ; i++) {
 			for (int j = 0 ; j < N; j++) {
+				int X = (int)map.get("X"+j);
+				String C = map.get("C"+j).toString();
 				if (cnt == X) {
 					//방향 전환이 일어남
-					if (C == 'D') {
-						lotate[i][j] = board[N-1-j][i];
-					} else if (C == 'L') {
-						
+					if ( C == "D") {
+						//오른쪽으로 방향 전환 = 왼쪽 90도 회전
+						board[i][j] = board[j][N-1-i];
+					} else if (C == "L") {
+						//왼쪽으로 방향 전환 = 오른쪽 90도 회전
+						board[i][j] = board[N-1-j][i];
 					} else {
 						System.out.println("오류 발생");;
+					}
+					if (board[i][j] == 1) {
+						System.out.println("사과 발견 "+i+","+j);
 					}
 				}
 				if (i == x && j == y) {
