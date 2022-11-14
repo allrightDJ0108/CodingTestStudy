ccbe77ccec38dfb058bc58b17ae002281348cbb2
Dajeong Choe <hers010801@gmail.com>
2022-11-14 23:44:17 +0900

    BaekJoon #1110 (난이도 : 하)

---
+++ CodingTestStudy1.0/src/Implementation/Problem1110.java
@@ -0,0 +1,34 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class Problem1110 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		int a, b, c = N;
+		
+		int cnt = 1;
+		
+		while(true) {
+			a = N / 10;		//십의자리
+			b = N % 10;		//일의자리
+			
+			a += b;
+			if (a >= 10) {
+				a = a % 10;
+			}
+			N = b*10 + (a);
+			
+			if (c == N) {
+				break;
+			} else {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+		scan.close();
+	}
+}
