b36816aad2b840ef0d6bdc4dcc43cfe375cd7db1
Dajeong Choe <hers010801@gmail.com>
2022-11-29 22:59:36 +0900

    ﻿BaekJoon #2588 #3003 #10430 #10171/10172 #25083 #1008 #15552 #2438 #25304

--- CodingTestStudy1.0/src/Implementation/Main.java
+++ CodingTestStudy1.0/src/Implementation/Main.java
@@ -4,40 +4,27 @@ package Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String str = br.readLine();
-		String[] array = str.split(" ");
 		
-		int A = numFn(Integer.parseInt(array[0]));
-		int B = numFn(Integer.parseInt(array[1]));
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		if (A > B) System.out.println(A);
-		else System.out.println(B);
-	}
-	
-	static int numFn(int x) {
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		String str = "";
+		String[] item = new String[2];
+		int sum = 0;
 		
-		for (int i=0;i<3;i++) {
-			map.put(i, x % 10);
-			x /= 10;
+		for (int i=0;i<N; i++) {
+			str = br.readLine();
+			item = str.split(" ");
+				
+			sum += Integer.parseInt(item[0]) * Integer.parseInt(item[1]);
 		}
-		x = 0;
-		int size = map.size()-1;
-		for (Integer key : map.keySet()){
-			x += map.get(key) * (int)Math.pow(10, size);
-			size--;
-	    }
 		
-		return x;
+		System.out.println(sum == X ? "Yes" : "No");
 	}
-	
 		
 }
 
