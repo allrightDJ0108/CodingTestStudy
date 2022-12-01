1002624376b3eb4cb54be140040b542ee1e29d1b
Dajeong Choe <hers010801@gmail.com>
2022-12-01 22:48:37 +0900

    ﻿BaekJoon #2741 #2742 #10871

--- CodingTestStudy1.0/src/Implementation/Problem10871.java
+++ CodingTestStudy1.0/src/Implementation/Problem10871.java
@@ -1,14 +1,36 @@
 package Implementation;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Problem10871 {
+	//방법1
+	/*
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String[] num = str.split(" ");
+		
+		int N = Integer.parseInt(num[0]);
+		int X = Integer.parseInt(num[1]);
+		
+		str = br.readLine();
+		String[] number = str.split(" ");
+		
+		for (int i=0;i<N;i++) {
+			
+			if (Integer.parseInt(number[i]) < X) {
+				System.out.print(number[i]);
+				System.out.print(" ");
+			}
+		}
+	}
+	*/
+	
+	//방법2
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder  sb = new  StringBuilder();
@@ -29,9 +51,4 @@ public class Main {
 		}
 		System.out.println(sb);
 	}
-		
 }
-
-	
-
-
