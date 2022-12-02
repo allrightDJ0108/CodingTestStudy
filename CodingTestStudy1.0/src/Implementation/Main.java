eb966809bdf2f48bc913da8c0833973eaabbd68c
Dajeong Choe <hers010801@gmail.com>
2022-12-03 00:02:00 +0900

    Baekjoon #11021

--- CodingTestStudy1.0/src/Implementation/Main.java
+++ CodingTestStudy1.0/src/Implementation/Main.java
@@ -11,23 +11,20 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder  sb = new  StringBuilder();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		int N = Integer .parseInt(st.nextToken());
-		int X = Integer .parseInt(st.nextToken());
+		int[] num = new int[T];
 		
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i=0; i<N; i++) {
-			int value = Integer.parseInt(st.nextToken());
-			
-			if(value < X) {
-				sb.append(value).append(" ");
-			}
+		String[] str = new String[2];
+		for(int i=0; i<T; i++) {
+			str = br.readLine().split(" ");
+			num[i] = Integer.parseInt(str[0])+Integer.parseInt(str[1]);
 		}
-		System.out.println(sb);
+		
+		for(int i=0; i<T; i++) {
+			System.out.println("Case #"+(i+1)+": "+num[i]);
+		}
 	}
 		
 }
