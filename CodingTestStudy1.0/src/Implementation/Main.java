13ead6fe767a5ce13fec1aa2fa5bfe6e36978f26
Dajeong Choe <hers010801@gmail.com>
2022-12-04 19:16:38 +0900

    Baekjoon #1546 #4344(N)

--- CodingTestStudy1.0/src/Implementation/Main.java
+++ CodingTestStudy1.0/src/Implementation/Main.java
@@ -4,29 +4,50 @@ package Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = Integer.parseInt(br.readLine());
+		int[] arr;
 		
-		int[] num = new int[T];
+		int testcase = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
-		String[] str = new String[2];
-		for(int i=0; i<T; i++) {
-			str = br.readLine().split(" ");
-			num[i] = Integer.parseInt(str[0])+Integer.parseInt(str[1]);
-		}
-		
-		for(int i=0; i<T; i++) {
-			System.out.println("Case #"+(i+1)+": "+num[i]);
+		for(int i = 0 ; i < testcase ; i++) {
+ 
+			st = new StringTokenizer(br.readLine()," "); // 학생 수 및 성적 입력
+			
+			
+			int N = Integer.parseInt(st.nextToken());	//학생 수 
+			arr = new int[N];
+			
+			double sum = 0;	// 성적 누적 합 변수 
+			
+			// 성적 입력부분 
+			for(int j = 0 ; j < N ; j++) {
+				int val = Integer.parseInt(st.nextToken());	// 성적 저장
+				arr[j] = val;
+				sum += val;	// 성적 누적 합 
+			}
+			
+			double mean = (sum / N) ;
+			double count = 0; // 평균 넘는 학생 수 변수 
+			
+			// 평균 넘는 학생 비율 찾기 
+			for(int j = 0 ; j < N ; j++) {
+				if(arr[j] > mean) {
+					count++;
+				}
+			}
+			
+			System.out.printf("%.3f%%\n",(count/N)*100);
+			
+	
 		}
 	}
-		
+	
 }
 
 	
