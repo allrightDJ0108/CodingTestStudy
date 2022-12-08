77e34ae8453aa7a93f7182043ac186fee054b70e
Dajeong Choe <hers010801@gmail.com>
2022-12-08 22:05:52 +0900

    Baekjoon #10951

--- CodingTestStudy1.0/src/Implementation/Problem10951.java
+++ CodingTestStudy1.0/src/Implementation/Problem10951.java
@@ -1,12 +1,11 @@
 package Implementation;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Problem10951 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -20,9 +19,4 @@ public class Main {
 		}
 		System.out.println(sb);
 	}
-	
 }
-
-	
-
-
