0dee29efa6169c8ff51023350ed416a0e15318cf
Dajeong Choe <hers010801@gmail.com>
2023-02-02 23:39:39 +0900

    Baekjoon #1436

--- CodingTestStudy1.0/src/Bruteforcing/Problem1436.java
+++ CodingTestStudy1.0/src/Bruteforcing/Problem1436.java
@@ -1,11 +1,10 @@
 package Bruteforcing;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Problem1436 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -28,10 +27,22 @@ public class Main {
 		
 		System.out.println(num);
 		
+		//처음에 접근했던 노답 방식,,^^;
+		/*
+		while(cnt < N) {
+			String[] str = String.valueOf(num).split("");
+			
+			for(int i=0; i < str.length; i++) {
+				if (str[str.length - 1].equals("6")) {
+					System.out.println(num);
+				}
+			}
+			num++;
+			cnt++;
+			
+		}
+		*/
+		
 	}
 	
 }
-
-	
-
-
