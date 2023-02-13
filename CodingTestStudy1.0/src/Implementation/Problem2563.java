3538ccbbaaccdd57c0f90028337a1bddb2382015
Dajeong Choe <hers010801@gmail.com>
2023-02-13 23:54:53 +0900

    Baekjoon #2563(ing)

---
+++ CodingTestStudy1.0/src/Implementation/Problem2563.java
@@ -0,0 +1,39 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem2563 {
+	//230213 막혔다,,,
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[100][100];
+		
+		for (int i=0; i<N; i++) {
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(str.nextToken());
+			int y = Integer.parseInt(str.nextToken());
+			
+			for (int a=x; a<x+10; x++) {
+				for (int b=y; b<y+10; b++) {
+					arr[a][b] = 1;
+				}
+			}
+			
+		}
+		
+		int cnt = 0;
+		for (int i=0; i<100; i++) {
+			for (int j=0; j<100; j++) {
+				if (arr[i][j] == 1) cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
