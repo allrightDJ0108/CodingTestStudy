162913d7cea414581fc75eb346917d6a60cc7264
Dajeong Choe <hers010801@gmail.com>
2023-02-22 00:00:19 +0900

    Baekjoon #10828

--- CodingTestStudy1.0/src/Stack/Problem10828.java
+++ CodingTestStudy1.0/src/Stack/Problem10828.java
@@ -4,55 +4,55 @@ import java.util.*;
 import java.io.*;
 
 public class Problem10828 {
-	//푸는중
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	    
 	    int N = Integer.parseInt(br.readLine());
+	    int[] stack = new int[N];
 	    int index = 0;
-	    Map<Integer, Integer> sta = new HashMap<Integer, Integer>();
-
 	    int in = 0;
 	    
-	    StringTokenizer str;
-	    while(index < N){
-	      str = new StringTokenizer(br.readLine());
-	      String order = str.nextToken();
-	      StringBuilder sb = new StringBuilder();
-	      
-	      if (order.equals("push") ) {
-	        sta.put(in, Integer.parseInt(str.nextToken()));
-	        in++;
-	        
-	      } else if (order.equals("pop") ) {
-	        if (in > 0){
-	          sb.append(sta.get(in-1));
-	          sta.put(in, null);
-	          in--;
-	        } else if (in == 0){
-	        		sta.put(in, null);
-	        		sb.append(-1);
-	          
-	        } else {
-	          sb.append(-1);
-	        }
-	        
-	      } else if (order.equals("size") ) {
-	        sb.append(in);
-	        
-	      } else if (order.equals("empty") ) {
-	        if (in > 0) sb.append(0); 
-	        else sb.append(1); 
-	      
-	      } else if (order.equals("top") ) {
-	        if (in > 0) sb.append(sta.get(in-1)); 
-	        else sb.append(-1); 
-	        
-	      }
-	      //sb.append("\n");
-	      System.out.println(sb);
-	      index++;    
-	    }
+	    StringTokenizer st;
+	    StringBuilder sb = new StringBuilder();
 	    
+	    while (index < N) {
+	    	st = new StringTokenizer(br.readLine());
+	    	String order = st.nextToken();
+	    	
+	    	if (order.equals("push")) {
+	    		stack[in] = Integer.parseInt(st.nextToken());
+	    		in++;
+	    		
+	    	} else if (order.equals("pop")) {
+	    		if ( in > 0 ) {
+	    			sb.append(stack[in-1]).append("\n");
+	    			stack[in-1] = 0;
+	    			in--;
+	    		} else if (in == 0) {
+	    			sb.append(-1).append("\n");
+	    		}
+	    		
+	    	} else if (order.equals("size")) {
+	    		sb.append(in).append("\n");
+	    		
+	    	} else if (order.equals("empty")) {
+	    		if (in > 0) {
+	    			sb.append(0).append("\n");
+	    		} else {
+	    			sb.append(1).append("\n");
+	    		}
+	    		
+	    	} else if (order.equals("top")) {
+	    		if (in > 0) {
+	    			sb.append(stack[in-1]).append("\n");
+	    		} else {
+	    			sb.append(-1).append("\n");
+	    		}
+	    		
+	    	}
+	    	
+	    	index++;
 	    }
+	    System.out.println(sb);
+	}
 }
