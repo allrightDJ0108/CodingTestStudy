c6c5ea2e18c4b7bef07d87c98fe152dca7e7fa63
Dajeong Choe <hers010801@gmail.com>
2023-02-28 23:18:41 +0900

    0228 testCommit

--- CodingTestStudy1.0/src/step01/Main.java
+++ CodingTestStudy1.0/src/step01/Main.java
@@ -1,41 +1,71 @@
 package step01;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
 	
-	public static void main(String[] args) {
-Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int x, y, N;
+		int N = Integer.parseInt(br.readLine());
+		String[] queue = new String[N];
+		StringTokenizer st;
+		int indexL = 0;
+		int indexR = 0;
+		int size = 0;
 		
-		x = scan.nextInt();
-		y = scan.nextInt();
-		
-		if (x > 0) {
-			N = (y > 0) ? 1 :  4;
-		} else {
-			N = (y > 0) ? 2 :  3;
-		}
-		
-
-		/*
-		if (x > 0) {
-			if (y > 0) {
-				N = 1;
-			} else {
-				N = 4;
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<N; i++) {
+			sb.setLength(0);
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			
+			if (str.equals("push")) {
+				queue[indexR] = st.nextToken();
+				indexR++;
+				size++;
+				
+			} else if (str.equals("pop")) {
+				if (size == 0) { //비어있는지 여부 확인
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(queue[indexL]).append("\n");
+					//queue[indexL] = "0";
+					indexL++;
+					size--;
+				}
+				
+			} else if (str.equals("size")) {
+				sb.append(size).append("\n");
+				
+			} else if (str.equals("empty")) {
+				if (size == 0) { //비어있는지 여부 확인
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
+				}
+				
+			} else if (str.equals("front")) {
+				if (size == 0) { //비어있는지 여부 확인
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(queue[indexL]).append("\n");
+				}
+				
+			} else if (str.equals("back")) {
+				if (size == 0) { //비어있는지 여부 확인
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(queue[indexR - 1]).append("\n");
+				}
+				
 			}
-		} else {
-			if (y>0) {
-				N = 2;
-			} else {
-				N = 3;
-			}
+			System.out.println(sb);
+			
 		}
-		*/
-		System.out.println(N);
-		scan.close();
-
 	}
 }
