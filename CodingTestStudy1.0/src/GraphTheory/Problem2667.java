6c31def24421097cc057ecc20a5b659ef6349d9c
Dajeong Choe <hers010801@gmail.com>
2023-03-21 22:22:28 +0900

    Baekjoon #2667

--- CodingTestStudy1.0/src/GraphTheory/Problem2667.java
+++ CodingTestStudy1.0/src/GraphTheory/Problem2667.java
@@ -4,38 +4,63 @@ import java.io.*;
 import java.util.*;
 
 public class Problem2667 {
-	static HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+	static StringBuilder sb = new StringBuilder();
+	static int[][] arr;
+	static int[][] visited;
+	static Queue<int[]> q = new LinkedList<>();
 	static int cnt = 0;
-	public static void main (String[] args) throws IOException{
+	static int temp = 0;
+	static int N;
+	static ArrayList<Integer> resArr = new ArrayList<Integer>();
+
+	static int[] dirX = { 1, -1, 0, 0 };
+	static int[] dirY = { 0, 0, 1, -1 };
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		for (int i=0; i<N; i++) {
-			String[] temp = br.readLine().split("");
-			for (int j=0; j<N; j++) {
-				if (map.get(i) == null) map.put(i, new ArrayList<Integer>());
-				map.get(i).add(Integer.parseInt(temp[j]));
+		N = Integer.parseInt(br.readLine());
+
+		arr = new int[N + 1][N + 1];
+		visited = new int[N + 1][N + 1];
+
+		for (int i = 0; i < N; i++) {
+			String[] str = br.readLine().split("");
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(str[j]);
 			}
 		}
-		
-		System.out.println(map);
-		
-		dfsFn(0);
-		
+
+		for (int x = 0; x < N + 1; x++) {
+			for (int y = 0; y < N + 1; y++) {
+				temp = 0;
+				if (arr[x][y] == 1 && visited[x][y] == 0) {
+					dfsFn(x, y);
+					resArr.add(temp);
+					cnt++;
+				}
+			}
+		}
+
+		Collections.sort(resArr);
+
+		System.out.println(resArr.size());
+		for (int i = 0; i < resArr.size(); i++) {
+			System.out.println(resArr.get(i));
+		}
 	}
-	
-	static void dfsFn(int R) {
-		
-		if (map.get(R) != null) {
-			for (int i=0; i<map.get(R).size(); i++) {
-				if (i+1 < map.get(R).size()) {
-					int a = map.get(R).get(i);
-					int b = map.get(R).get(i+1);
-					if (a == 1 && b == 1) cnt++;
+
+	static void dfsFn(int x, int y) {
+		visited[x][y] = 1;
+		temp++;
+		for (int i = 0; i < 4; i++) {
+			int newX = x + dirX[i];
+			int newY = y + dirY[i];
+
+			if (newX >= 0 && newX < N && newY >= 0 && newY < N) {
+				if (arr[newX][newY] == 1 && visited[newX][newY] == 0) {
+					dfsFn(newX, newY);
 				}
 			}
 		}
 	}
-
 }
