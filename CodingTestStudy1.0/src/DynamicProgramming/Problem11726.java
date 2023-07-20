0702066cff0af2c2ec7ab8f3853469ebe85750f9
Dajeong Choe <hers010801@gmail.com>
2023-07-20 17:17:20 +0900

    Baekjoon #11726

---
+++ CodingTestStudy1.0/src/DynamicProgramming/Problem11726.java
@@ -0,0 +1,22 @@
+package DynamicProgramming;
+
+import java.io.*;
+
+public class Problem11726 {
+	
+	static int[] d = new int[1000+1];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		d[1] = 1;
+		d[2] = 2;
+		for (int i=3; i<N+1; i++) {
+			d[i] = (d[i-1] + d[i-2]) % 10007;
+		}
+		
+		System.out.println(d[N]);
+	}
+}
