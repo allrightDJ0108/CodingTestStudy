716336a6fc7b6ad817806a5407ad3319c46c559d
Dajeong Choe <hers010801@gmail.com>
2023-07-27 11:45:31 +0900

    Baekjoon #19238(ing)

--- CodingTestStudy1.0/src/Samsung/Problem19238.java
+++ CodingTestStudy1.0/src/Samsung/Problem19238.java
@@ -4,152 +4,152 @@ import java.io.*;
 import java.util.*;
 
 public class Problem19238 {
-	static int N, M, oil, cnt = 0;
-	static int[][] map, start, dest;
-	static int[] taxi;
 	
+	static int N, M, S;
+	static int[][] map;
+	static int sx, sy;
+	static int[][] info;
+	static Queue<int[]> q = new LinkedList<>();
+	static int[][] dir = {{0,-1}, {0,1}, {1,0}, {-1,0}};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer str = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(str.nextToken());
+		M = Integer.parseInt(str.nextToken());
+		S = Integer.parseInt(str.nextToken());
 		
-		N = Integer.parseInt(st.nextToken()); // 행, 열 크기 
-		M = Integer.parseInt(st.nextToken()); // 승객 수 
-		oil = Integer.parseInt(st.nextToken()); // 연료 
-		map = new int[N][N];
-		
-		start = new int[M+1][2]; // 승객들의 출발지 좌표 리스트 
-								// (EX: 1번 손님의 출발지 x좌표는 start[1][0], y좌표는 start[1][1]) 
-		dest = new int[M+1][2]; // 승객들의 도착지 좌표 리스트 
-		taxi = new int[2]; // 택시의 현재 위치 
-		
-		for(int i=0; i< N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 1) { // 벽은 -1로 바꿔준다. (0 이상이면 승객으로 처리할 것이기 때문)
-					map[i][j] = -1;
-				}
+		map = new int[N+1][N+1];
+		for (int i=1; i<N+1; i++) {
+			str = new StringTokenizer(br.readLine());
+			int temp = 0;
+			for (int j=1; j<N+1; j++) {
+				temp = Integer.parseInt(str.nextToken());
+				if (temp == 1) temp = 9999;
+				map[i][j] = temp;
 			}
 		}
 		
-		st = new StringTokenizer(br.readLine());
-		taxi[0] = Integer.parseInt(st.nextToken()) -1; // 0~ N-1 좌표를 가져야하기 때문에 -1 해준다. 
-		taxi[1] = Integer.parseInt(st.nextToken()) -1;
-
-		for(int i=1; i<=M; i++) {
-			st = new StringTokenizer(br.readLine());
-			start[i][0] = Integer.parseInt(st.nextToken()) -1;
-			start[i][1] = Integer.parseInt(st.nextToken()) -1;
-			map[start[i][0]][start[i][1]] = i; // 승객의 출발지 위치에 승객 번호를 지정 (겹치지 X) 
-			dest[i][0] = Integer.parseInt(st.nextToken()) -1; // 다른 승객의 출발지 위치와 겹칠 수 있기 때문에 map에 지정하지 않는다.
-			dest[i][1] = Integer.parseInt(st.nextToken()) -1;
+		str = new StringTokenizer(br.readLine());
+		sx = Integer.parseInt(str.nextToken());
+		sy = Integer.parseInt(str.nextToken());
+		
+		info = new int[M+1][5];				//0번칸은 거리 계산 후 대입
+		for (int i=1; i<M+1; i++) {
+			str = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(str.nextToken());
+			int y = Integer.parseInt(str.nextToken());
+			int gx = Integer.parseInt(str.nextToken());
+			int gy = Integer.parseInt(str.nextToken());
+			map[x][y] = i;					//그래프에 위치 표시
+			map[gx][gy] = i + 10000;		//M의 최대값은 N^2이므로 절대 나오지 않을 숫자를 더해 목적지 설정
+			
+			info[i][1] = x;
+			info[i][2] = y;
+			info[i][3] = gx;
+			info[i][4] = gy;
+			
 		}
 		
-		int answer = -1;
-		while(true) {
-			int[] passengerInfo = bfs(taxi[0], taxi[1]); // [0]: 승객번호, [1]: 거리
-			// 승객 운반 및 오일 충전
-			move(passengerInfo);
+		
+		while (S > 0) {
+			int useS = 0;
 			
-			if(passengerInfo[0] == 0 || oil < 0) break; // 승객이 없거나 || 연료가 다 닳았으면 끝.
-
-			cnt++;
-			if(cnt == M) { // 모든 승객을 다 운반했으면
-				answer = oil;
+			//가장 짧은 거리와 승객 선택
+			int thisNo = 10000;
+			int thisIdx = 0;
+			for (int i=1; i<M+1; i++) {
+				if (info[i][0] != 11111) {
+					info[i][0] = distanceFunc(sx,sy,info[i][1],info[i][2]);		//거리 계산
+					if (thisNo > info[i][0]) {
+						thisNo = info[i][0];
+						thisIdx = i;
+					}
+				}
+			}
+			
+			if (thisNo == 10000 && thisIdx == 0) {
 				break;
 			}
-		}
-		System.out.println(answer);
-	}
-
-	private static void move(int[] near) {
-		// 거리 구하기
-		int p = near[0];
-		int dir = getDistance(p);
-		// oil 양 줄인 후 충전
-		oil -= dir + near[1];
-		if(oil < 0) return;
-		oil += (dir * 2);
-		// 승객 초기화
-		map[start[p][0]][start[p][1]] = 0; 
-		// 택시 위치 옮기기 
-		taxi[0] = dest[p][0];
-		taxi[1] = dest[p][1];
-	}
-
-	private static int getDistance(int p) { // p의 출발지부터 도착지까지의 최소 거리 구하기 
-		int minDir = Integer.MAX_VALUE; // 최소 거리 
-		boolean[][] visited = new boolean[N][N];
-		Deque<int[]> deque = new ArrayDeque<>();  
-		deque.add(new int[] {start[p][0], start[p][1], 0}); // x 좌표, y 좌표, 출발지부터의 거리
-		visited[start[p][0]][start[p][1]] = true;
-		int[] dx = {-1, 0, 1, 0};
-		int[] dy = {0, 1, 0, -1};
-		
-		while(!deque.isEmpty()) {
-			int[] now = deque.poll();
-			for(int d=0; d<4; d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-				
-				if(nx >= N || nx < 0 || ny >= N || ny < 0) continue;
-				if(map[nx][ny] == -1 || visited[nx][ny]) continue;
-				if(nx == dest[p][0] && ny == dest[p][1]) { // 도착지이면 
-					if(now[2]+1 < minDir) // 최소거리 갱신 
-						minDir = now[2]+1;
-				} else if(!visited[nx][ny]) {
-					deque.add(new int[] {nx, ny, now[2]+1});
-					visited[nx][ny] = true;
-				}
+			if (thisNo == -1) {
+				S = -1;
+				break;
 			}
+			
+			//승객에게 이동
+			S = usingfFunc(S, info[thisIdx][0], 1);
+			
+			//목적지까지 이동					//1,2는 출발지 3,4는 목적지
+			useS = distanceFunc(info[thisIdx][1], info[thisIdx][2], info[thisIdx][3], info[thisIdx][4]);
+			//이동 후 남은 연료
+			S = usingfFunc(S, useS, 1);
+			if (S >= 0) {	
+				//이동에 성공 시 소모 연료량 두 배 충전
+				S = usingfFunc(S, useS, 2);
+			} else {
+				//이동에 실패 시 -1 반환
+				S = -1;
+			}
+			
+			
+			info[thisIdx][0] = 11111;		//이동이 완료된 경우 완료 처리
+			sx = info[thisIdx][3];
+			sy = info[thisIdx][4];
+			
+			
 		}
-		return minDir;
+		
+		
+		System.out.println(S);
+		
+		
+		
 	}
-
-	// 택시와 가장 가까운 승객 정보 반환
-	// param: 택시의 좌표
-	// return: 가장 가까운 승객의 정보 [0]: 승객 번호, [1]: 거리 
-	private static int[] bfs(int x, int y) {
-		if(map[x][y] > 0) { // 승객과 택시가 같은 곳에 있는 경우 
-			return new int[] {map[x][y], 0};
+	
+	//연료 계산하는 함수
+	static int usingfFunc(int cur, int use, int sol) {
+		if (sol == 1) {		//승객 또는 목적지로 이동하는 경우
+			cur = cur - use;
+		} else {			//목적지 도달에 성공한 경우
+			cur = cur + (use * 2);
 		}
 		
-		int near = 0; // 가장 가까운 승객 
-		int minDir = Integer.MAX_VALUE; // 가까운 승객과의 거리
-		boolean[][] visited = new boolean[N][N];
-		Deque<int[]> deque = new ArrayDeque<>();
-		deque.add(new int[] {x, y, 0});
-		visited[x][y] = true;
-		int[] dx = {-1, 0, 1, 0};
-		int[] dy = {0, 1, 0, -1};
+		if (cur < 0) cur = -1;
+		return cur;
+	}
+	
+	//최단거리 구하는 함수
+	static int distanceFunc(int x, int y, int gx, int gy) {
+		q = new LinkedList<>();
+		q.add(new int[]{x, y});
 		
-		while(!deque.isEmpty()) {
-			int[] now = deque.poll();
-			for(int d=0; d<4; d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-
-				if(nx >= N || nx < 0 || ny >= N || ny < 0) continue;
-				if(map[nx][ny] == -1 || visited[nx][ny]) continue;
-				if(map[nx][ny] > 0) { // 승객이면 
-					if(now[2]+1 < minDir) { // 거리가 최소면
-						minDir = now[2]+1; // 거리 갱신 
-						near = map[nx][ny]; // 내가 제일 가까운 승객 
-					} else if(now[2]+1 == minDir) { // 택시로부터 최소 거리 승객과 같은 거리면 
-						if(nx < start[near][0]) { // 행이 더 작으면 
-							near = map[nx][ny]; // 내가 제일 가까운 승객 
-						} else if(nx == start[near][0] && ny < start[near][1]) { // 행은 같은데 열이 더 작으면  
-							near = map[nx][ny]; // 내가 제일 가까운 승객
+		int[][] visited = new int[N+1][N+1];
+		visited[x][y] = 1;
+		
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int curX = cur[0];
+			int curY = cur[1];
+			for (int i=0; i<4; i++) {
+				int nextX = curX + dir[i][0];
+				int nextY = curY + dir[i][1];
+				if (nextX > 0 && nextY > 0 && nextX <= N && nextY <= N) {
+					if (nextX == gx && nextY == gy) {
+						return visited[curX][curY];
+					}
+					if (map[nextX][nextY] != 9999) {
+						if (map[nextX][nextY] >= 0 && visited[nextX][nextY] == 0) {
+							q.add(new int[] {nextX, nextY});
+							visited[nextX][nextY] = visited[curX][curY] + 1;
 						}
 					}
 				}
-				
-				if(!visited[nx][ny]) {
-					deque.add(new int[] {nx, ny, now[2]+1});
-					visited[nx][ny] = true;
-				}
 			}
 		}
-		return new int[] {near, minDir};
+		
+		return -1;
 	}
+
+
 }
