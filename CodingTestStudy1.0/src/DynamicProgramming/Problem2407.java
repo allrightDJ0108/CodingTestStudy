844d6889e70b83836f05580777f7bb0b714dcd1d
Dajeong Choe <hers010801@gmail.com>
2023-07-31 12:46:21 +0900

    Baekjoon #2407(DP로 푸는 방법 추가)

--- CodingTestStudy1.0/src/DynamicProgramming/Problem2407.java
+++ CodingTestStudy1.0/src/DynamicProgramming/Problem2407.java
@@ -5,6 +5,8 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Problem2407 {
+	
+	static BigInteger[][] bi = new BigInteger[100+1][100+1];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,7 +24,22 @@ public class Problem2407 {
 		}
 
 		System.out.println(sum.divide(div));
+		dpFunc(N,M);			//DP를 이용한 풀이
 
 	}
 
+	
+	static void dpFunc(int N, int M) {
+		for (int i=1;i <= N; i++) {
+			for (int j=0; j <= i; j++) {
+				if (j==0 || j == i) {
+					bi[i][j] = BigInteger.ONE;
+				} else {
+					bi[i][j] = bi[i-1][j].add(bi[i-1][j-1]);
+				}
+			}
+		}
+		
+		System.out.println(bi[N][M]);
+	}
 }
