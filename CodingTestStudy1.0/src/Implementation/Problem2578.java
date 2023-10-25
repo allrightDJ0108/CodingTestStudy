164140961f383b2a0bf15dca96c7198f6a0607b1
Dajeong Choe <hers010801@gmail.com>
2023-10-25 23:56:54 +0900

    Baekjoon #2578 빙고

--- CodingTestStudy1.0/src/Implementation/Problem2578.java
+++ CodingTestStudy1.0/src/Implementation/Problem2578.java
@@ -4,106 +4,119 @@ import java.io.*;
 import java.util.*;
 
 public class Problem2578 {
-    static int[][] arr;
-    static int count = 0;
-    static boolean flagR = false; 
-    static boolean flagL = false;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer str;
+	static int[][] arr;
+	static int count = 0;
+	static boolean flagR = false;
+	static boolean flagL = false;
 
-        arr = new int[5][5];
-        for (int i=0; i<5; i++){
-            str = new StringTokenizer(br.readLine());
-            for (int j=0; j<5; j++){
-                arr[i][j] = Integer.parseInt(str.nextToken());
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str;
 
-        int ind = 0;
-        for (int i=0; i<5; i++){
-            str = new StringTokenizer(br.readLine());
-            for (int j=0; j<5; j++){
-                int aws = Integer.parseInt(str.nextToken());
-                ind++;
-                for (int a=0; a<5; a++){
-                    for (int b=0; b<5; b++){
-                        if (arr[a][b] == aws) {
-                            arr[a][b] = 0;
-                            count += bingo(a,b);
-                            if (!flagR) count += bingoR();
-                            if (!flagL) count += bingoL();
-                        }
-                        if (count == 3){
-                            i=6; j=6;
-                        }
-                    }
-                }
-            }
-        }
+		arr = new int[5][5];
+		for (int i = 0; i < 5; i++) {
+			str = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 5; j++) {
+				arr[i][j] = Integer.parseInt(str.nextToken());
+			}
+		}
 
-        System.out.println(ind);
+		int ind = 0;
+		for (int i = 0; i < 5; i++) {
+			str = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 5; j++) {
+				int aws = Integer.parseInt(str.nextToken());
+				ind++;
+				for (int a = 0; a < 5; a++) {
+					for (int b = 0; b < 5; b++) {
+						if (arr[a][b] == aws) {
+							arr[a][b] = 0;
+						}
+					}
+				}
+				
+				bingoX();
+				bingoY();
+				bingoR();
+				bingoL();
 
-    }
+				//System.out.println(count);
+				if (count >= 3) {
+					System.out.println(ind);
+					return;
+				}
+				count = 0;
+			}
+		}
 
-    static int bingo(int x, int y){
-        int cnt = 0;
-        int tempX = 0;
-        int tempY = 0;
-        for (int i=0; i<5; i++){
-            
-            if (arr[x][i] == 0){
-                tempX++;
-            }
+	}
 
-            if (arr[i][y] == 0){
-                tempY++;
-            }
+	static void bingoX() {
+		
+		for (int i = 0; i < 5; i++) {
+			int tempX = 0;
+			for (int j = 0; j < 5; j++) {
+				if (arr[i][j] == 0) {
+					tempX++;
+				}
+			}
+			//System.out.printf("tempX %d ", tempX );
+			if (tempX == 5) {
+				count++;
+			}
+		}
 
-        }
+		
+		
 
-        if (tempX == 5){
-            cnt++;
-        }
-        if (tempY == 5){
-            cnt++;
-        }
+	}
 
-        return cnt;
-    }
-    
-    static int bingoR(){
-        int cnt = 0;
-        int tempR = 0;
-        for (int i=0; i<5; i++){
-            if (arr[i][i] == 0){
-                tempR++;
-            }
-        }
-        if (tempR == 5){
-            cnt++;
-            flagR = true;
-        }
-        
-        return cnt;
-    }
-    
-    static int bingoL(){
-        int cnt = 0;
-        int tempL = 0;
-        int j=4;
-        for (int i=0; i<5; i++, j--){
-            if (arr[i][j] == 0){
-                tempL++;
-            }
-        }
+	static void bingoY() {
+		for (int i = 0; i < 5; i++) {
+			int tempY = 0;
+			for (int j = 0; j < 5; j++) {
+				if (arr[j][i] == 0) {
+					tempY++;
+				}
+			}
+			//System.out.printf("tempY %d ", tempY );
+			if (tempY == 5) {
+				count++;
+			}
+		}
 
-    	if (tempL == 5){
-            cnt++;
-            flagL = true;
-        }
-        
+		
 
-        return cnt;
-    }
+	}
+
+	static void bingoR() {
+		int tempR = 0;
+		for (int i = 0; i < 5; i++) {
+			if (arr[i][i] == 0) {
+				tempR++;
+			}
+		}
+		
+		//System.out.printf("tempR %d ", tempR );
+		if (tempR == 5) {
+			count++;
+		}
+
+	}
+
+	static void bingoL() {
+		int tempL = 0;
+		int j = 4;
+		for (int i = 0; i < 5; i++, j--) {
+			if (arr[i][j] == 0) {
+				tempL++;
+			}
+		}
+
+		//System.out.printf("tempL %d \n", tempL );
+		if (tempL == 5) {
+			count++;
+		}
+
+	}
 }
