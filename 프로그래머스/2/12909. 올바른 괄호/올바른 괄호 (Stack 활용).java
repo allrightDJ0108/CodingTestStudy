ab537380f9d17f849f33b571e4ad7b443de370f8
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-03-29 17:06:55 +0900

    [level 2] Title: 올바른 괄호 Stack 활용 풀이

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호 (Stack 활용).java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) == '('){
+                stack.push(1);
+            } else {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
