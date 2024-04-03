30860d1674cc8d9d6c471440e0234261554a936a
Dajeong Choe <hers010801@gmail.com>
2024-04-03 11:54:35 +0900

    Cos Pro 1급 5차 6번 풀이

--- Cos Pro/기출문제/1급 5차/문제/5차 1급 6_initial_code.java
+++ Cos Pro/기출문제/1급 5차/문제/5차 1급 6_initial_code.java
@@ -1,13 +1,41 @@
 // 다음과 같이 import를 사용할 수 있습니다.
 import java.util.*;
 
-class Solution {	
+class Solution {
     public String solution(String s1, String s2, int p, int q) {
         // 여기에 코드를 작성해주세요.
         String answer = "";
+        int num1 = 0;
+        int num2 = 0;
+
+        // p진법의 숫자를 10진수로 변환 - 변환 시 지수가 반대로 되는 것 주의! (s.length() - 1 - i)
+        for (int i=0; i<s1.length(); i++){
+            num1 += (s1.charAt(i)-'0') * Math.pow(p,s1.length() - 1 - i);
+        }
+        for (int i=0; i<s2.length(); i++){
+            num2 += (s2.charAt(i)-'0') * Math.pow(p,s2.length() - 1 - i);
+        }
+
+        int num = num1 + num2;
+
+        // 10진수를 다시 q진법으로 변환
+        StringBuilder sb = new StringBuilder();
+        while (true){
+            sb.append(num % q);
+            num /= q;
+
+            if (num < q){
+                sb.append(num % q);
+                break;
+            }
+        }
+
+        answer = sb.reverse().toString();
+
         return answer;
     }
-    
+
+
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.	
     public static void main(String[] args) {
     	Solution sol = new Solution();
