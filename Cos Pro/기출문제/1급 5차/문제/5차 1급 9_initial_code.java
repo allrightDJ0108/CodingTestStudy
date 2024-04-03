d03c999db8b959f2cbf19fbe6063ef3af78ab56f
Dajeong Choe <hers010801@gmail.com>
2024-04-03 11:39:01 +0900

    Cos Pro 1급 5차 9번 오답 풀이

--- Cos Pro/기출문제/1급 5차/문제/5차 1급 9_initial_code.java
+++ Cos Pro/기출문제/1급 5차/문제/5차 1급 9_initial_code.java
@@ -3,43 +3,44 @@ import java.util.*;
 
 class Solution {
 	public int solution(int number, int target) {
-		// 여기에 코드를 작성해주세요.
 		int answer = 0;
 
-		int num = number;
+		//visited에는 index를 만들기 위한 연산 횟수가 저장
+		int[] visited = new int[10001];
 
-		int cnt = 0;
-		int small = 0;
-		int big = 0;
+		Queue<Integer> q = new LinkedList<Integer>(); //연산의 결과를 넣을 큐(3개씩 나옴)
+		q.add(number);
+		visited[number]=1; //visited에 number를 방문했음을 기록
 
-		while (true){
-			num = num * 2;
-			cnt++;
-			if (num >= target) {
-				small = num / 2;
-				big = num;
-				break;
+		while(!q.isEmpty()){
+			int x = q.poll();
+
+			//check(visited, target);
+			if(x == target) break; //q에서 가져온 값이 타겟과 같으면 종료
+
+			if(x+1 <=10000 &&  visited[x+1]==0){  //x+1이 방문한 적이 없으면
+				visited[x+1]=visited[x]+1; //visited[x]의 방문 횟수에 1을 더해서 visited[x+1]에 저장
+				q.add(x+1); //q에 연산 결과를 추가
+			}
+			if(x-1 >=0 &&  visited[x-1]==0){  //x-1이 방문한 적이 없으면
+				visited[x-1]=visited[x]+1; //visited[x]의 방문 횟수에 1을 빼서 visited[x+1]에 저장
+				q.add(x-1); //q에 연산 결과를 추가
+			}
+			if(x*2 <=10000 &&  visited[x*2]==0){  //x*2이 방문한 적이 없으면
+				visited[x*2]=visited[x]+1; //visited[x]의 방문 횟수에 2을 곱해서 visited[x+1]에 저장
+				q.add(x*2); //q에 연산 결과를 추가
 			}
 		}
+		// target을 만들기 위한 연산 횟수
+		answer=visited[target]-1;
+		return answer;
+	}
 
-		System.out.println(num +" "+cnt+" "+small +" "+big);
-
-		// 차이가 더 작은 쪽을 선택
-		if (target - small <= big - target) {
-			cnt--;
-			while (small != target){
-				small += 1;
-				cnt++;
-			}
-		} else {
-			while (big != target){
-				big -= 1;
-				cnt++;
-			}
+	static void check(int[] visited, int target){
+		for (int i=0; i<=target; i++){
+			System.out.print(visited[i]+" ");
 		}
-
-
-		return cnt;
+		System.out.println();
 	}
 
 	// 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
