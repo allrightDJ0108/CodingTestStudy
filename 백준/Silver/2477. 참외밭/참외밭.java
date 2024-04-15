2e281aa462d82a360334cf269803841263dcbb92
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-15 09:56:14 +0900

    [Silver II] Title: 참외밭, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -14,7 +14,8 @@ public class Main {
         
         int maxR = Integer.MIN_VALUE;
         int maxC = Integer.MIN_VALUE;
-        int indexR = 0; int indexC = 0;
+        int indexR = 0; 
+        int indexC = 0;
         int[] inp = new int[6];
         
         for (int i=0; i<6; i++) {
@@ -33,16 +34,10 @@ public class Main {
         	inp[i] = len;
         }
         
-        int nextR1 = inp[5], nextC1=inp[5]; // nextR1,R2 => 세로길이 후보
-		int nextR2 = inp[0], nextC2=inp[0]; // nextC1,C2 => 가로길이 후보
-		if (indexC-1>-1) nextR1=inp[indexC-1];
-		if (indexC+1<6) nextR2=inp[indexC+1];
-		if (indexR-1>-1) nextC1=inp[indexR-1];
-		if (indexR+1<6) nextC2=inp[indexR+1];
-		
-		// 최대 가로길이*세로길이후보 중 작은 길이 + 가로길이후보 중 작은 길이*(최대 세로길이-세로길이후보 중 작은 길이)
-		int area = maxC * Math.min(nextR1, nextR2) + Math.min(nextC1, nextC2) * (maxR - (Math.min(nextR1, nextR2)));
-		area = area * K;
+        int maxSquare = maxR * maxC;
+        int minSquare = inp[(indexR + 3) % 6] * inp[(indexC + 3) % 6];
+        
+		int area = (maxSquare - minSquare) * K;
 		
         System.out.println(area);
         
