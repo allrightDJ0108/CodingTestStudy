92664f135458e5f84f72903f830c4d0d1d64c7cc
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-16 16:27:39 +0900

    Update 오픈채팅방.java

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -4,7 +4,7 @@ class Solution {
     public ArrayList<String> solution(String[] record) {
         
         HashMap<String, String> map = new HashMap<>();
-        ArrayList<String> answer = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>(); // id : 닉네임
         
         for (String r : record){
             StringTokenizer str = new StringTokenizer(r, " ");
@@ -13,23 +13,27 @@ class Solution {
             String user = str.nextToken();
             
             if (func.equals("Enter")){
+                // log에 id로 기록
                 answer.add(user+"님이 들어왔습니다.");
                 
+                // id에 대한 닉네임 저장
                 String nickname = str.nextToken();
                 map.put(user, nickname);
                 
             } else if ( func.equals("Leave") ){
+                // log에 id로 기록
                 answer.add(user+"님이 나갔습니다.");
                 
             } else if ( func.equals("Change") ){
+                // id에 대한 수정된 닉네임 저장
                 String nickname = str.nextToken();
                 map.put(user, nickname);
             }
             
-            
         }
         
-        for (int i=0; i<answer.size(); i++){
+        // id를 닉네임으로 바꿔주기
+        for (int i=0; i < answer.size(); i++){
             String[] log = answer.get(i).split("님이 ");
             String id = log[0];
             String action = log[1];
