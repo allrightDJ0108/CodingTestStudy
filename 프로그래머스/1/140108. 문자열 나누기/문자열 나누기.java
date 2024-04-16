9639239fbdc451ce5d76f28c80442abd723794c6
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-16 17:16:54 +0900

    Update 문자열 나누기.java

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -8,6 +8,7 @@ class Solution {
         int same = 0;
         int diff = 0;
         for (int i=0; i<arr.length; i++){
+            // x를 분리된 문자열의 첫 글자로 지정
             if (x.equals("")) x = arr[i];
             
             if (x.equals(arr[i])) same++;
@@ -15,11 +16,13 @@ class Solution {
             
             if (same == diff) {
                 answer++;
+                // 기존 변수들을 초기화
                 same = 0;
                 diff = 0;
                 x = "";
             }
-            
+
+            // 문자열을 나누기 전에 종료되면 마지막 문자열을 개수에 포함시킴
             if (i == arr.length - 1 && same != diff) answer++;
         }
         return answer;
