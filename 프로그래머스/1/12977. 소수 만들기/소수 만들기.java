e648309dd08eadd269f25f03f86b6e01d27982ec
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-17 14:56:32 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+       for (int i=0; i < nums.length - 2; i++){
+           for (int j=i+1; j < nums.length - 1; j++){
+               for (int k=j+1; k < nums.length; k++){
+                   int sum = nums[i] + nums[j] + nums[k];
+                   if (isPrime(sum)) answer++;
+               }
+           }
+       }
+
+        return answer;
+    }
+    
+    static boolean isPrime(int n){
+        
+        int sq = (int) Math.sqrt(n);
+        
+        for (int i=2; i <= sq; i++){
+            if (n % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
