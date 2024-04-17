f33ab943907f823e1ad58dd4522baedfdfe3b003
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-17 22:57:49 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 584 ms, Memory: 64496 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,58 +4,54 @@ import java.util.*;
 public class Main {
 	
 	static int N;
-	static ArrayList<Integer>[] arr;
+	static ArrayList<Integer>[] list;
+	static int[] visited;
 	static Queue<Integer> q = new LinkedList<>();
-	static boolean[] visited;
-	static int[] parent;
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str;
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new ArrayList[N+1];
-		for (int i=1; i<N+1; i++) {
-			arr[i] = new ArrayList<>();
-		}
-		
-		visited = new boolean[N+1];
-		parent = new int[N+1];
-		
-		for (int i=0; i<N-1; i++) {
-			str = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(str.nextToken());
-			int y = Integer.parseInt(str.nextToken());
-			arr[x].add(y);
-			arr[y].add(x);
-		}
-		
-		bfsFn(1);
-		
-		for (int i=2; i<N+1; i++) {
-			sb.append(parent[i]).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	static void bfsFn(int x) {
-		q.add(x);
-		visited[x] = true;
-		
-		while(!q.isEmpty()) {
-			int t = q.poll();
-			
-			for (int temp : arr[t]) {
-				if (!visited[temp]) {
-					parent[temp] = t;
-					visited[temp] = true;
-					q.add(temp);
-				}
-				
-			}
-		}
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str;
+        StringBuilder sb = new StringBuilder();
+        
+        N = Integer.parseInt(br.readLine());
+        
+        visited = new int[N+1];
+        list = new ArrayList[N+1];
+        for (int i=0; i<N+1; i++) {
+        	list[i] = new ArrayList<>();
+        }
+        
+        for (int i=0; i<N-1; i++) {
+        	str = new StringTokenizer(br.readLine());
+        	
+        	int x = Integer.parseInt(str.nextToken());
+        	int y = Integer.parseInt(str.nextToken());
+        	
+        	list[x].add(y);
+        	list[y].add(x);
+        }
+        
+        q.add(1);
+        visited[1] = 0;
+        
+        while (!q.isEmpty()) {
+        	int cur = q.poll();
+        	
+        	for (int next : list[cur]) {
+        		if (visited[next] == 0) {
+        			q.add(next);
+        			visited[next] = cur;
+        		}
+        	}
+        }
+        
+        for (int i=2; i<N+1; i++) {
+        	sb.append(visited[i]).append("\n");
+        }
+        
+        System.out.println(sb);
+        
+    }
+    
+    
 }
