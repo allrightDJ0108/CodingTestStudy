912358e3d9ee831815b420b6bbc9bcac4fbd4be0
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-17 17:22:36 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        String answer = "";
+        
+        int[] numX = new int[10];
+        int[] numY = new int[10];
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        // X의 숫자들을 숫자 배열에 카운트
+        for (int i=0; i<X.length(); i++){
+            int n = X.charAt(i) - '0';
+            numX[n]++;
+        }
+        //func(numX);
+        
+        // Y의 숫자들을 숫자 배열에 카운트
+        for (int i=0; i<Y.length(); i++){
+            int n = Y.charAt(i) - '0';
+            numY[n]++;
+        }
+        //func(numY);
+        
+        // X, Y 배열에서 숫자가 0보다 크면 작은값 만큼 배열에 넣어줌
+        for (int i=9; i>=0; i--){
+            if (numX[i] > 0 && numY[i] > 0){
+                int min = Math.min(numX[i], numY[i]);
+                for (int j=0; j<min; j++){
+                    list.add(i);
+                }
+            }
+        }
+        
+        // 배열을 String으로 만들어주기
+        StringBuilder sb = new StringBuilder();
+        for (int i : list){
+            sb.append(i);
+        }
+        
+        answer = sb.toString();
+        
+        if (answer.equals("")) answer = "-1";
+        else if (answer.charAt(0) == '0') answer = "0";
+        
+        return answer;
+    }
+    
+    static void func(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
