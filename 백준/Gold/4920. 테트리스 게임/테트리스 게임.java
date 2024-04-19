9e06e01bece24f235b28d45d0342e91667b48914
DJ._.Dazzling <44766266+allrightDJ0108@users.noreply.github.com>
2024-04-19 10:08:17 +0900

    [Gold V] Title: 테트리스 게임, Time: 280 ms, Memory: 19060 KB -BaekjoonHub

--- 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
+++ 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
@@ -1,95 +1,94 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int[][][] figure = {
-        {
-            {0, 0, 0, 0}, {0, 1, 2, 3}
-        },
-        {
-            {0, 1, 2, 3}, {0, 0, 0, 0}
-        },
-
-        {
-            {0, 0, 1, 1}, {0, 1, 1, 2}
-        },
-        {
-            {0, 1, 1, 2}, {0, 0, -1, -1}
-        },
-
-        {
-            {0, 0, 0, 1}, {0, 1, 2, 2}
-        },
-        {
-            {0, 1, 2, 2}, {0, 0, 0, -1}
-        },
-        {
-            {0, 1, 1, 1}, {0, 0, 1, 2}
-        },
-        {
-            {0, 0, 1, 2}, {0, 1, 0, 0}
-        },
-
-        {
-            {0, 1, 1, 1}, {0, -1, 0, 1}
-        },
-        {
-            {0, 1, 1, 2}, {0, 0, 1, 0}
-        },
-        {
-            {0, 0, 0, 1}, {0, 1, 2, 1}
-        },
-        {
-            {0, 1, 1, 2}, {0, -1, 0, 0}
-        },
-
-        {
-            {0, 0, 1, 1}, {0, 1, 1, 0}
-        },
-
-    };
-    static int[][] map;
-    static int N;
-
-
-    public static void main(String[] args) throws IOException {
+	
+	static int N;
+	static int[][] board;
+	static int[][][] blocks = {
+			{{0,0},{0,1},{0,2},{0,3}},
+			{{0,0},{1,0},{2,0},{3,0}},
+			
+			{{0,0},{0,1},{1,1},{1,2}},
+			{{0,1},{1,0},{1,1},{2,0}},
+			
+			{{0,0},{0,1},{0,2},{1,2}},
+			{{0,1},{1,1},{2,0},{2,1}},
+			{{0,0},{1,0},{1,1},{1,2}},
+			{{0,0},{0,1},{1,0},{2,0}},
+			
+			{{0,0},{0,1},{0,2},{1,1}},
+			{{0,1},{1,0},{1,1},{2,1}},
+			{{0,1},{1,0},{1,1},{1,2}},
+			{{0,0},{1,0},{1,1},{2,0}},
+			
+			{{0,0},{0,1},{1,0},{1,1}},
+	};
+	
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = 1;
-        while ((N = Integer.parseInt(br.readLine().trim())) != 0) {
-            map = new int[N][N];
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine().trim());
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
+        StringTokenizer str;
+        StringBuilder sb = new StringBuilder();
+        
+        int testCase = 1;
+        
+        while (true) {
+        	
+        	N = Integer.parseInt(br.readLine().trim());
+        	if (N == 0) break;
+            board = new int[N][N];
+            
+            for (int i=0; i<N; i++) {
+            	str = new StringTokenizer(br.readLine()," ");
+            	for (int j=0; j<N; j++) {
+            		String s = str.nextToken();
+            		board[i][j] = Integer.parseInt(s);
+            	}
             }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    for (int a = 0; a < figure.length; a++) {
-                        int sum = 0;
-                        boolean flag = true;
-                        for (int b = 0; b < 4; b++) {
-                            int nx = i + figure[a][0][b];
-                            int ny = j + figure[a][1][b];
-                            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                                sum += map[nx][ny];
-                            } else {
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if (flag) {
-                            max = Math.max(max, sum);
-                        }
-                    }
-                }
-            }
-            System.out.println(input + ". " + max);
-            input++;
+            
+            int answer = play();
+            
+            sb.append(testCase++ +". "+answer).append("\n");
         }
+        
+        System.out.println(sb);
+        
     }
+    
+    static int play() {
+    	int maxSum = Integer.MIN_VALUE;
+    	
+    	for (int i=0; i<N; i++) {
+    		for (int j=0; j<N; j++) {
+    			
+    			for (int b=0; b<blocks.length; b++) {
+    				
+    				int sum = 0;
+    				boolean flag = true;
+    				
+    				for (int c=0; c<4; c++) {
+    					int nx = i + blocks[b][c][0];
+    					int ny = j + blocks[b][c][1];
+    					
+    					if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+    						sum += board[nx][ny];
+    					} else {
+    						flag = false;
+    						break;
+    					}
+    					
+    				}
+    				
+    				if (flag) {
+    					maxSum = Math.max(maxSum, sum);
+    				}
+					
+    			}
+    			
+    		}
+    	}
+    	
+    	return maxSum;
+    }
+    
 }
